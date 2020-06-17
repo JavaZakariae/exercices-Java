6da74fb7a85f00e43dfa347d86b739014fd6002e
bouali zakariae <bouali_zakariae@hotmail.fr>
2020-06-17 13:48:04 +0200

    responses for the suite algos

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,44 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] firstKElementsSuite = firstKElementsGeomSuite(2, 8, 4);
+        Arrays.stream(firstKElementsSuite)
+              .forEach((e)-> System.out.println(e));
+    }
+
+
+    /**
+     *
+     * @return the first k elements of an arithmetic  suite
+     */
+    public static int [] firstKElementsArithSuite( int depart, int raison, int k){
+        int [] firstKValues = new int[k];
+        int counter=0;
+        int currentValue = depart;
+        while (counter < k){
+            firstKValues[counter]=currentValue;
+            currentValue += raison ;
+            counter++;
+        }
+        return firstKValues;
+    }
+    /**
+     *
+     *
+     * @return the first k elements of a geometric suite
+     */
+    public static int [] firstKElementsGeomSuite( int depart, int raison, int k){
+
+        int [] firstKValues = new int[k];
+        int counter=0;
+        int currentValue = depart;
+        while (counter < k){
+            firstKValues[counter]=currentValue;
+            currentValue *= raison ;
+            counter++;
+        }
+        return firstKValues;
     }
 }
